c7d4c4561fa4b54072bc8d24f5136c07d138ad26
yolothedino <darhdevil@hotmail.com>
2019-10-18 18:45:23 +0800

    lots of changes

--- schoolLocations.py
+++ schoolLocations.py
@@ -1,17 +1,32 @@
+"""
+Main dictionary can be accessed with:
+sch_data_dict
+Keys: School Names
+[0]: Postal Code
+[1]: Latitude
+[2]: Longitude
+[3]: Distance to input postal code (default 0)
+[4]: Whether school is near (default True)
+Call distance_calc_all(<postal code of home>) to update [3]
+Call is_it_near(<max distance>) to update [4] (only after updating [3])
+    is_it_near(<max distance)[0] returns list sorted school's distances
+    is_it_near(<max distance)[1] returns list of schools sorted by their distances
+"""
 import pandas as pd
 from geopy.distance import geodesic
+from collections import OrderedDict
 
 # importing CSV files
 sch_data = pd.read_csv('general-information-of-schools.csv', usecols=['school_name', 'postal_code', 'zone_code',
                                                                       'mainlevel_code'])
 cords_data = pd.read_csv("geonames-coordinates.csv", usecols=['postal_code', 'Latitude', 'Longitude'])
 cca_data = pd.read_csv("co-curricular-activities-ccas.csv", usecols=['school_name', 'cca_grouping_desc',
-                                                                 'cca_generic_name'])
-subjects_data = pd.read_csv("subjects-offered.csv", usecols=['school_name', 'subject_desc'])
+                                                                  'cca_generic_name'])
+# subjects_data = pd.read_csv("subjects-offered.csv", usecols=['school_name', 'subject_desc'])
 
 # joining some columns and rows together
 # cca_data_grouped = cca_data.groupby(['school_name', 'cca_grouping_desc'])['cca_generic_name'].apply(', '.join).reset_index()
-# cca_data_all = cca_data_grouped.groupby(['school_name'])['cca_generic_name'].apply(', '.join).reset_index()
+cca_data = cca_data.groupby(['school_name'])['cca_generic_name'].apply(', '.join).reset_index()
 # cca_data_groups = cca_data_grouped.groupby(['school_name'])['cca_grouping_desc'].apply(', '.join).reset_index()
 # subjects_data = subjects_data.groupby(['school_name'])['subject_desc'].apply(', '.join).reset_index()
 
@@ -21,23 +36,7 @@ sch_data = sch_data.merge(cords_data, on="postal_code", how='left').fillna(0)  #
 # sch_data = sch_data.merge(cca_data_groups, on="school_name", how='left').fillna(0)  # adds cca groups to main data
 # sch_data = sch_data.merge(cca_data_all, on="school_name", how='left').fillna(0)  # adds actual ccas to main data
 
-
-def cords_from_name(*sch_name):
-    """
-    Getting coordinates with school name
-    Used only for postal codes in Singapore
-    Returns tuple(s) in the form of (latitude, longitude)
-    """
-    cords = []
-    for i in sch_name:
-        for j in i: #dont ask me why but it only works in a double loop
-            index_no = sch_data[sch_data['school_name'] == j].index.item()
-            lat = sch_data.loc[index_no, "Latitude"]
-            lon = sch_data.loc[index_no, "Longitude"]
-            cord_tuple = (float(lat),float(lon))
-            cords.append(cord_tuple)
-    return cords
-
+sch_data_dict = {}
 
 def cords_from_postal(p_code):
     """
@@ -49,23 +48,13 @@ def cords_from_postal(p_code):
     lat = cords_data.loc[index_no, "Latitude"]
     lon = cords_data.loc[index_no, "Longitude"]
     return [float(lat), float(lon)]
-
-def distance_calc(p_code_start, p_code_end):
-    """
-    Returns distance from home
-    Takes in start and end postal codes
-    """
-    start = cords_from_postal(p_code_start)
-    end = cords_from_postal(p_code_end)
-    distance = geodesic(start, end).km
-    return distance
-
 def distance_calc_all(start_code):
     """
     Updates main dataframe with distance to all schools as new column
     """
     closest_up = start_code
     closest_down = start_code
+    ori_code = start_code
     while start_code not in list(cords_data['postal_code']): #uses next closest available postal code
         closest_up += 1
         closest_down -= 1
@@ -73,23 +62,67 @@ def distance_calc_all(start_code):
             start_code = closest_up
         elif closest_down in list(cords_data['postal_code']):
             start_code = closest_down
-    print start_code
-    distances_postal_codes = list(sch_data['postal_code'].values)
-    distances = []
-    for i in distances_postal_codes:
-        distances.append(distance_calc(start_code, int(i)))
-    sch_data['distances']=distances
+    if start_code != ori_code:
+        print '{} not found, used next closest available postal code: {}'.format(ori_code, start_code)
+    for i in sch_data_dict:
+        sch_data_dict[i][3] = round(geodesic(cords_from_postal(start_code),
+                                             (sch_data_dict[i][1],sch_data_dict[i][2])).km, 2)
+def is_it_near(max_distance):
+    new_dict = {}
+    sch_list = []
+    dist_list = []
+    for i in sch_data_dict:
+        if sch_data_dict[i][3] <= max_distance:
+            sch_data_dict[i][4] = True
+            new_dict[i] = sch_data_dict[i]
+            new_dict[i][0] = sch_data_dict[i][0]
+            new_dict[i][1] = sch_data_dict[i][1]
+            new_dict[i][2] = sch_data_dict[i][2]
+            new_dict[i][3] = sch_data_dict[i][3]
+            new_dict[i][4] = sch_data_dict[i][4]
+            sch_list.append(i)
+            dist_list.append(sch_data_dict[i][3])
+        elif max_distance == 0:
+            sch_data_dict[i][4] = True
+        else:
+            sch_data_dict[i][4] = False
+    to_return = sort_two_lists_2gd(dist_list,sch_list)
+    return to_return[0], to_return[1]
+
+def sort_two_lists_2gd(values,names):
+    counter = 0
+    while counter < len(values)-1:
+        counter = 0
+        for i in range(len(values)-1):
+            if values[i]>values[i+1]:
+                temp1 = values[i]
+                temp2 = values[i+1]
+                tempa = names[i]
+                tempb = names[i+1]
+                values[i] = temp2
+                values[i+1] = temp1
+                names[i] = tempb
+                names[i+1] = tempa
+            else:
+                counter += 1
+    return values, names
 
 
-def filter_distance(max_distance):
-    """
-    based on max_distance (in km), calculate schools that are within radius
-    returns sorted values in (dataframe, list of schools, list of distances)
-    """
-    is_max_dis = sch_data['distances'] <= max_distance
-    filtered_data = sch_data[is_max_dis].sort_values('distances')
-    filtered_data = filtered_data.reset_index(drop=True)
-    return filtered_data, list(filtered_data['school_name']), list(filtered_data.round(2)['distances'])
-
-
-all_cords = cords_from_name(sch_data['school_name'])
+sch_data_sch_name = sch_data['school_name'].to_list()
+sch_data_pcode = sch_data['postal_code'].to_list()
+sch_data_lat = sch_data['Latitude'].to_list()
+sch_data_lon = sch_data['Longitude'].to_list()
+for i in range(len(sch_data_sch_name)):
+    sch_data_dict[sch_data_sch_name[i]] = [int(sch_data_pcode[i]), float(sch_data_lat[i]), float(sch_data_lon[i]),
+                                           float(0),True]
+sch_cca_names = cca_data['school_name'].to_list()
+sch_cca_ccas = cca_data['cca_generic_name'].to_list()
+sch_ccas = {}
+for i in range(len(sch_cca_names)):
+    sch_ccas[sch_cca_names[i]] = sch_cca_ccas[i].split(', ')
+    for x in range(len(sch_ccas[sch_cca_names[i]])):
+        if sch_ccas[sch_cca_names[i]][x] == 'MUSIC':
+            sch_ccas[sch_cca_names[i]][x] = 'MUSIC, DRAMA & DANCE CLUB'
+        elif sch_ccas[sch_cca_names[i]][x] == 'DRAMA & DANCE CLUB':
+            sch_ccas[sch_cca_names[i]][x] = ''
+    sch_ccas[sch_cca_names[i]] = [x for x in sch_ccas[sch_cca_names[i]] if not '']
